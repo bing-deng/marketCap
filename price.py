e0561936334614ba6b91a64122e83c8618cb6f93
dengbb <dengbb@dengbbs-MacBook-Pro.local>
2020-02-04 16:24:56 +0900

    init

--- price.py
+++ price.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import xlwt # write excel 新建一个Excel文件（只能通过新建写入）
 import time
 import math
+from datetime import datetime
 
 data = xlwt.Workbook()
 
@@ -33,24 +34,41 @@ class MC():
         
 
     def get_data(self):
-
-        url = "https://coinmarketcap.com/currencies/" + self.coin +"/historical-data/?start=20200106&end=20200203"
+        #start=20200101&end=20200202"
+        url = "https://coinmarketcap.com/currencies/" + self.coin +"/historical-data/?start=20191201&end=20200202"
         print(url)
         headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
         req = Request(url=url, headers=headers)
         html = urlopen(req).read()
 
         bsObj = BeautifulSoup(html,"html.parser")
+
+        # 
+
+        date_list = bsObj.findAll('td', class_='cmc-table__cell cmc-table__cell--sticky cmc-table__cell--left')
+        date_str_list = []
+        for i in date_list:
+            d = i.text.replace(",","")
+            datetime_object = datetime.strptime(d, '%b %d %Y')
+            date_str_list.append(datetime_object)
+            # print(str(datetime_object))
+        
         tr_list = bsObj.findAll('td', class_='cmc-table__cell cmc-table__cell--right')
         j = 1
         k = 0
-        print(len(bsObj.findAll('tr', class_='cmc-table-row')))
+        print("===")
+        print(len(date_str_list))
+        print(len(tr_list))
+        print("===")
         for index, i in enumerate(tr_list):
             text = i.text.replace(",","")
             if index % 6 == 0:
-                print("")
+                print("---")
                 j = j + 1
+                print(index)
+                self.table.write(j,0,str(date_str_list[int(index / 6) ]))
                 k = 0
+                
             # if k < 4 or k == 5:
             #     price = float(text) * rate
             #     price = "%.4f" % float(price) 
@@ -60,7 +78,8 @@ class MC():
             price = float(text) * rate
             price = "%.4f" % float(price) 
             price = price.replace(",","")
-            self.table.write(j,k,price)
+            self.table.write(j,k+1,price)
+            
             k = k + 1
         
            
@@ -69,10 +88,10 @@ if __name__ == "__main__":
     
     try:
         # stellar xml
-        # coin_list = ["bitcoin", "bitcoin-cash", "ethereum","litecoin", "xrp", "monacoin", "ethereum-classic"]
+        coin_list = ["bitcoin", "bitcoin-cash", "ethereum","litecoin", "xrp", "monacoin", "ethereum-classic"]
         coin_list = ["nem", "lisk", "bitcrystals", "comsa-eth", "factom", "pepe-cash", "qash", "storjcoin-x","counterparty"]
-        coin_list = ["tether", "binance-coin", "eos", "bitcoin-sv"]
-        coin_list = ["horizen","dogecoin","stellar"]
+        coin_list = ["tether", "binance-coin", "eos", "bitcoin-sv","horizen","dogecoin","stellar"]
+        
 
         for i in coin_list:
             mc = MC(i)
